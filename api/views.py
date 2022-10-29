c85aa1a70d1e9b683387b11a86f64e519a5a146e
Phil Bell <phil@legl.com>
2022-10-29 20:55:58 +0100

    updating questset filters

--- api/views.py
+++ api/views.py
@@ -9,26 +9,29 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_questset(self):
+        self.queryset.filter(pk=self.request.user.pk)
+
 
 class ServerViewset(viewsets.ModelViewSet):
     queryset = Server.objects.all()
-    serializer = ServerSerializer
+    serializer_class = ServerSerializer
 
     def get_queryset(self):
-        self.queryset.filter(user=self.user)
+        self.queryset.filter(user=self.request.user)
 
 
 class LocationViewset(viewsets.ModelViewSet):
     queryset = Location.objects.all()
-    serializer = LocationSerializer
+    serializer_class = LocationSerializer
 
     def get_queryset(self):
-        self.queryset.filter(server__user=self.user)
+        self.queryset.filter(server__user=self.request.user)
 
 
 class TorrentViewset(viewsets.ModelViewSet):
     queryset = Torrent.objects.all()
-    serializer = TorrentSerializer
+    serializer_class = TorrentSerializer
 
     def get_queryset(self):
-        self.queryset.filter(server__user=self.user)
+        self.queryset.filter(server__user=self.request.user)
