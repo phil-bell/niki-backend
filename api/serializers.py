17dedd27de6cfeceea6d43d72f51bae071a83158
Phil Bell <phil@legl.com>
2022-11-06 13:35:39 +0000

    adding user validation mixin

--- api/serializers.py
+++ api/serializers.py
@@ -3,27 +3,40 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+class UserValidatedMixin:
+    def get_user_attribute(self, attr) -> User:
+        return attr.get("user")
 
+    def validate(self, attr):
+        if self.context["request"].user != self.get_user_attribute(attr):
+            raise serializers.ValidationError("User does not own this server")
+        return super().validate(attr)
+
+
+class UserSerializer(serializers.ModelSerializer, UserValidatedMixin):
     class Meta:
         model = User
         fields = ["pk", "url", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
 
-class ServerSerializer(serializers.ModelSerializer):
+class ServerSerializer(serializers.ModelSerializer, UserValidatedMixin):
     class Meta:
         model = Server
         fields = [
             "pk",
             "url",
             "name",
-            "user",
             "secret",
         ]
 
+    def create(self, validated_data) -> Server:
+        server = super().create(validated_data)
+        server.update(user=self.context["request"].user)
+        return server
 
-class LocationSerializer(serializers.ModelSerializer):
+
+class LocationSerializer(serializers.ModelSerializer, UserValidatedMixin):
     class Meta:
         model = Location
         fields = [
@@ -33,8 +46,11 @@ class LocationSerializer(serializers.ModelSerializer):
             "server",
         ]
 
+    def get_user_attribute(self, attr) -> User:
+        return attr.get("server").user
 
-class TorrentSerializer(serializers.ModelSerializer):
+
+class TorrentSerializer(serializers.ModelSerializer, UserValidatedMixin):
     class Meta:
         model = Torrent
         fields = [
@@ -44,3 +60,6 @@ class TorrentSerializer(serializers.ModelSerializer):
             "server",
             "location",
         ]
+
+    def get_user_attribute(self, attr) -> User:
+        return attr.get("server").user
