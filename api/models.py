42196fae84c57fc0e366e8b2acaa2a50642c57f9
Phil Bell <phil@legl.com>
2022-12-12 18:08:31 +0000

    adding users, key and secret fields to server

--- api/models.py
+++ api/models.py
@@ -1,13 +1,24 @@
 import uuid
 
 from django.contrib.auth.models import User
+from django.core.management.utils import get_random_secret_key
 from django.db import models
+
+
+def generate_secret():
+    return secrets.token_urlsafe(255)
 
 
 class Server(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    secret = models.UUIDField(default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(
+        User,
+        related_name="owned_servers",
+        on_delete=models.CASCADE,
+    )
+    key = models.UUIDField(default=uuid.uuid4)
+    secret = models.CharField(default=generate_secret, max_length=255)
+    users = models.ManyToManyField(User, related_name="servers")
 
 
 class Location(models.Model):
