c03c5ce0a0917141484922a8782066f64cf10bd7
Philip Bell <philhabell@gmail.com>
2023-09-03 19:14:32 +0100

    removing AuthMiddlewareStack

--- niki/asgi.py
+++ niki/asgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -22,6 +21,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        "websocket": URLRouter(websocket_urlpatterns),
     }
 )
