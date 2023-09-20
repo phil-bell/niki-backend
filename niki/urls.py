140acbdfd0b44f0c88d78c073cab963a2773c898
Philip Bell <philhabell@gmail.com>
2023-09-20 21:38:24 +0100

    fixinf admin url

--- niki/urls.py
+++ niki/urls.py
@@ -23,7 +23,7 @@ router.register(r"torrent", TorrentViewset)
 router.register(r"key", KeyViewset, basename="key")
 
 urlpatterns = [
-    path("/", admin.site.urls),
+    path("", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
