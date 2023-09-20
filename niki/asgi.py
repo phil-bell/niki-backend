f1560e1b15e60ad75b7d59c6b4ea98063168f52f
Philip Bell <philhabell@gmail.com>
2023-09-20 21:29:46 +0100

    fixing error from url patterns being imported too early

--- niki/asgi.py
+++ niki/asgi.py
@@ -12,11 +12,11 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from niki.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "niki.settings")
 
 django_asgi_app = get_asgi_application()
+
+from niki.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
